f00f1fd333fd5aef16853ca41f0b6f014a280435
Ruth Peace Ibitomisin <37203661+omofumike@users.noreply.github.com>
2022-06-24 09:24:15 +0100

    Add files via upload

---
+++ introduction.js
@@ -0,0 +1,4 @@
+// Write a javascript code that prints out your name, height, and country on the screen
+console.log('Ruth Peace Ibitomisin')
+console.log('Height: 1.64m');
+console.log('Country: Nigeria');
